a18fad8441ae1629f261d59ac8cb117e49bd86d3
Henrique <henriquecampanhagarcia@gmail.com>
2025-07-25 18:57:40 -0300

    [/] Updating the View UI of the entities

--- lua/entities/commsarray/cl_init.lua
+++ lua/entities/commsarray/cl_init.lua
@@ -20,10 +20,13 @@ function ENT:Draw()
 		ang.y = LocalPlayer():EyeAngles().y - 90
 		if LocalPlayer():GetPos():DistToSqr( self:GetPos() ) < 2048 * 2048 then
 			local hp_percent = self:Health() / self:GetMaxHealth()
+			hp_percent = hp_percent * 10000
+			hp_percent = math.Round( hp_percent ) / 100
+			hp_percent = hp_percent * 0.01 -- Convert to a percentage.
 			cam.Start3D2D( p + Vector( 5, 0, 10 ), Angle( 0, ang.y, 90 ), .15 )
-				draw.RoundedBox( 5, - 180, - 550, 350, 50, Color( 0, 0, 0, 230 ) )
-				draw.RoundedBox( 5, - 180 + 2, - 550 + 2, math.Clamp( hp_percent * 350, 0, 350 ), 50 - 2, Color( 150, 0, 0, 230 ) )
-				draw.DrawText( hp_percent * 100 .. "%", "SystemsHealth", 5, - 540, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER )
+				draw.RoundedBox( 5, - 180, - 300, 350, 50, Color( 0, 0, 0, 230 ) )
+				draw.RoundedBox( 5, - 180 + 2, - 300 + 2, math.Clamp( hp_percent * 350, 0, 350 ), 50 - 2, Color( 150, 0, 0, 230 ) )
+				draw.DrawText( hp_percent * 100 .. "%", "SystemsHealth", 5, - 290, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER )
 			cam.End3D2D()
 		end
 	end
